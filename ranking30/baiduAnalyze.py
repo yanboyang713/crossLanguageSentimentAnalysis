70e7185f932b12eef6eb6ec455ab02c423bae6fb
Boyang Yan <yanboyang713@gmail.com>
2017-12-20 08:24:26 +1100

    baidu sentment analysis base on baidu translated data

--- ranking30/baiduAnalyze.py
+++ ranking30/baiduAnalyze.py
@@ -15,12 +15,12 @@ def main():
 
     for line_counter in range(input_selection[0], input_selection[1]):
         try:
-            sentence = sheet.cell(row=line_counter, column=8).value
+            sentence = sheet.cell(row=line_counter, column=24).value
             baidu.analyze(sentence)
-            sheet.cell(row = line_counter, column = 13).value = baidu.getPositiveProb()
-            sheet.cell(row = line_counter, column = 14).value = baidu.getConfidence()
-            sheet.cell(row = line_counter, column = 15).value = baidu.getNegativeProb()
-            sheet.cell(row = line_counter, column = 16).value = baidu.getSentiment()
+            sheet.cell(row = line_counter, column = 27).value = baidu.getPositiveProb()
+            sheet.cell(row = line_counter, column = 28).value = baidu.getConfidence()
+            sheet.cell(row = line_counter, column = 29).value = baidu.getNegativeProb()
+            sheet.cell(row = line_counter, column = 30).value = baidu.getSentiment()
         except Exception as exception:
             print (exception)
             continue
