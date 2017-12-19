48ca19b3cd82c74d63210c0945510e2127af0cf2
Boyang Yan <yanboyang713@gmail.com>
2017-12-19 17:55:00 +1100

    update readme

---
+++ mysql.py
@@ -0,0 +1,6 @@
+import mysql.connector
+
+cnx = mysql.connector.connect(user='scott', password='tiger',
+                              host='127.0.0.1',
+                              database='employees')
+cnx.close()
