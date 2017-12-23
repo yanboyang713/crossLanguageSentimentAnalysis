030a0b792c8ff3f394a67d86c5d03ce41c4449a5
Boyang Yan <yanboyang713@gmail.com>
2017-12-23 13:53:41 +1100

    add Baidu chinese postitive Probability To Google Score stadard for origin data

---
+++ ranking10/postitiveProbabilityToGoogleScore.py
@@ -0,0 +1,39 @@
+import openpyxl
+import sys
+from decimal import *
+def main():
+    fileName = 'Ranking10.xlsx'
+    # Select which lines of the input sentences you wish to use
+    input_selection = [1, 8573]
+    input_column = 9
+    output_column = 31
+    try:
+        file = openpyxl.load_workbook(fileName)
+    except:
+        file = openpyxl.Workbook()
+
+    sheet = file['Sheet1']
+
+    for line_counter in range(input_selection[0], input_selection[1]):
+        try:
+            inputValue = sheet.cell(row=line_counter, column=input_column).value
+            if inputValue:
+                inputValue = Decimal(inputValue)
+                getcontext().prec = 30
+                if (inputValue < 0.5):
+                    result = (1 - inputValue) * -1
+                elif (inputValue > 0.5):
+                    result = inputValue
+                else:
+                    result = 0
+                sheet.cell(row = line_counter, column = output_column).value = Decimal(result)
+        except Exception as exception:
+            print (exception)
+            continue
+    # Save the file and notify the user
+    file.save(fileName)
+    print("change finish")
+
+
+if __name__ == "__main__":
+    main()
