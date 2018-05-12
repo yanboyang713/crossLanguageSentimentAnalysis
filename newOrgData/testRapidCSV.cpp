c8cc179e28de01a1d71e6f435071af568ca8bcbd
Boyang Yan <yanboyang713@gmail.com>
2018-05-12 15:09:29 +1000

    update

--- newOrgData/testRapidCSV.cpp
+++ newOrgData/testRapidCSV.cpp
@@ -4,17 +4,15 @@
 
 int main()
 {
-  rapidcsv::Document doc("goodDataWithHeader.csv");
-
+    rapidcsv::Document doc("goodDataWithHeaderStartFromRanking.csv");
   std::vector<float> close = doc.GetColumn<float>("Baidu postitive probability change to Google score standard");
   std::cout << "Read " << close.size() << " values." << std::endl;
   for (int i = 0; i < close.size(); i++){
       std::cout << close[i] << std::endl;
 
   }
-  /*
-  long long volume = doc.GetCell<long long>("Volume", "2011-03-09");
-  std::cout << "Volume " << volume << " on 2011-03-09." << std::endl;
-  */
-  // ...
+      /*
+  long long volume = doc.GetCell<long long>("Baidu postitive probability change to Google score standard", "10");
+  std::cout << "Volume " << volume << " on ranking 10." << std::endl;
+      */
 }
