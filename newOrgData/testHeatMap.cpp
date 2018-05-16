0f117a1e59765434069a24d6fe7de4131d47e3ae
Boyang Yan <yanboyang713@gmail.com>
2018-05-16 18:11:42 +1000

    update heatmap

--- newOrgData/testHeatMap.cpp
+++ newOrgData/testHeatMap.cpp
@@ -1,23 +1,17 @@
 #include <iostream>
 #include <vector>
-#include <string>
 #include "rapidcsv.h"
 using namespace std;
 
 int findIndex(float, float*&, const int&);
-int main()
-{
-    rapidcsv::Document doc("goodDataLatest.csv", rapidcsv::LabelParams(0, -1));
-    std::vector<int> ranking = doc.GetColumn<int>("ranking");
-    std::vector<float> googleSentimentOriginData = doc.GetColumn<float>("Google Chinese sentiment analysis score(base on Chinese origin data)");
-    std::vector<float> googleSentimentGoogleTranslatedData = doc.GetColumn<float>("Google English sentiment analysis score(base on Google translated data)");
-    std::vector<float> googleSentimentYandexTranslatedData = doc.GetColumn<float>("Google English sentiment analysis score(base on Yandex translated data)");
-    std::vector<float> googleSentimentBaiduTranslatedData = doc.GetColumn<float>("Google English sentiment analysis score(base on Baidu translated data)");
+int main(){
 
-    std::vector<float> baiduSentimentOriginData = doc.GetColumn<float>("Baidu Chinese sentiment analysis positive probability(base on Chinese origin data)");
-    std::vector<float> baiduSentimentGoogleTranslatedData = doc.GetColumn<float>("Baidu English sentiment analysis positive probability(base on Google translated data)");
-    std::vector<float> baiduSentimentYandexTranslatedData = doc.GetColumn<float>("Baidu English sentiment analysis postitive probability(base on Yandex translated data)");
-    std::vector<float> baiduSentimentBaiduTranslatedData = doc.GetColumn<float>("Baidu English sentiment analysis postitive probability(base on Baidu translated data)");
+    rapidcsv::Document doc("goodDataLatest.csv", rapidcsv::LabelParams(0, -1));
+    //std::vector<float> googleSentimentOriginData= {-0.9, -0.2, 0.3, 0.4, 0.3};
+
+    std::vector<float> googleSentimentOriginData = doc.GetColumn<float>("Google Chinese sentiment analysis score(base on Chinese origin data)");
+    //std::vector<float> baiduSentimentOriginData = {-0.9, -0.1, 0.3, 0.5, 0.3};
+    std::vector<float> baiduSentimentOriginData = doc.GetColumn<float>("Baidu positive probability to google standard(base on Chinese origin data)");
 
     float interval = 0.1;
     int size = static_cast<int>(2 / interval + 1);
@@ -26,10 +20,11 @@ int main()
     float* xAndYindex = new float[size];
     xAndYindex[0] = -1.0;
     for (int i = 1; i < size; ++i) {
-        xAndYindex[i] = xAndYindex[i - 1] + 0.1;
+        xAndYindex[i] = xAndYindex[i - 1] + interval;
         cout << xAndYindex[i] << endl;
-
         }
+
+
     // dynamic allocation
     int** dataMatrix = new int*[size];
     for(int i = 0; i < size; ++i)
@@ -38,45 +33,47 @@ int main()
     for(int i = 0; i < size; ++i)
         for(int j = 0; j < size; ++j)
             dataMatrix[i][j] = 0;
+    int rowCount = googleSentimentOriginData.size();
 
-    size_t rowCount = doc.GetRowCount();
     for (int i = 0; i < rowCount; ++i) {
 
-    //for (int i = 0; i < 5; ++i) {
         float baiduData = baiduSentimentOriginData[i];
         float googleData = googleSentimentOriginData[i];
 
         int baiduIndex = findIndex(baiduData, xAndYindex, size);
         int googleIndex = findIndex(googleData, xAndYindex, size);
         cout << "baiduIndex = " << baiduIndex << endl;
+        cout << "baiduData = " << baiduData << endl;
+
         cout << "googleIndex = " << googleIndex << endl;
-
-
-
-        std::cout << dataMatrix[baiduIndex][googleIndex] << endl;
-
+        cout << "googleData = " << googleData << endl;
 
         dataMatrix[baiduIndex][googleIndex]+= 1;
-
-        std::cout << dataMatrix[baiduIndex][googleIndex] << endl;
-            }
+    }
 
     // print
     cout <<"size = " << size << endl;
-    for(int y = size; y > 0; y--){
+
+    for(int y = size - 1; y >= 0; y--){
         for(int x = 0; x < size; ++x){
             std::cout << dataMatrix[x][y] << " ";
         }
         cout << endl;
     }
 
+
     // free
     for(int i = 0; i < size; ++i)
         delete [] dataMatrix[i];
     delete [] dataMatrix;
+
+
+
+
     return 0;
 
 }
+
 int findIndex(float data, float*& xAndYindex, const int& size){
     int index = -1;
     //cout << "data = " << data << endl;
