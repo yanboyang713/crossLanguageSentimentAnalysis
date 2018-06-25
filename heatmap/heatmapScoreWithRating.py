9ffb21e1be8634c1526055fffda056cf5362c4d3
Boyang <yanboyang713@gmail.com>
2018-06-25 17:08:55 +1000

    backup

--- heatmap/heatmapScoreWithRating.py
+++ heatmap/heatmapScoreWithRating.py
@@ -22,6 +22,28 @@ heatmap = np.array([[0, 0, 0, 0, 0],
                     [0, 0, 0, 0, 0],
                     [0, 0, 0, 0, 0],
                     [0, 0, 0, 0, 0]])
+
+newHeatmap = np.array([[0, 0, 0, 0, 0],
+                    [0, 0, 0, 0, 0],
+                    [0, 0, 0, 0, 0],
+                    [0, 0, 0, 0, 0],
+                    [0, 0, 0, 0, 0],
+                    [0, 0, 0, 0, 0],
+                    [0, 0, 0, 0, 0],
+                    [0, 0, 0, 0, 0],
+                    [0, 0, 0, 0, 0],
+                    [0, 0, 0, 0, 0],
+                    [0, 0, 0, 0, 0],
+                    [0, 0, 0, 0, 0],
+                    [0, 0, 0, 0, 0],
+                    [0, 0, 0, 0, 0],
+                    [0, 0, 0, 0, 0],
+                    [0, 0, 0, 0, 0],
+                    [0, 0, 0, 0, 0],
+                    [0, 0, 0, 0, 0],
+                    [0, 0, 0, 0, 0],
+                    [0, 0, 0, 0, 0]])
+
 ranking = np.array([])
 GooogleScoreOriginData = np.array([])
 GoogleScoreGoogleTranslatedData = np.array([])
@@ -64,60 +86,11 @@ with open('goodDataLatest.csv', "r") as csvfile:
         index += 1
 
 def divideScoresIntoTwentyRegions(num):
-    print ("before num = ", num)
-
     num = np.around(num, decimals = 1)
-    print ("after num = ", num)
     if num == -1:
         return 0
     elif num == 1:
         return 19
-    elif num == -0.8:
-        print ("*******")
-    '''
-    if num == -0.9 or num == -1:
-        return 0
-    elif num == -0.8:
-        return 1
-    elif num == 0.7:
-        return 2
-    elif num == -0.6:
-        return 3
-    elif num == -0.5:
-        return 4
-    elif num == -0.4:
-        return 5
-    elif num == -0.3:
-        return 6
-    elif num == -0.2:
-        return 7
-    elif num == -0.1:
-        return 8
-    elif num == 0.0:
-        return 9
-    elif num == 0.1:
-        return 10
-    elif num == 0.2:
-        return 11
-    elif num == 0.3:
-        return 12
-    elif num == 0.4:
-        return 13
-    elif num == 0.5:
-        return 14
-    elif num == 0.6:
-        return 15
-    elif num == 0.7:
-        return 16
-    elif num == 0.8:
-        return 17
-    elif num == 0.9:
-        return 18
-    elif num == 1:
-        return 19
-    np.around(num, decimals = 1)
-    '''
-    print ("return = ", int(math.floor((num + 1) * 10)) - 1)
     return np.around((num + 1) * 10 - 1, decimals = 1)
 def divedeRanking(ranking):
     if ranking == 10:
@@ -137,19 +110,24 @@ for index in range(0, ranking.size):
     temp = divedeRanking(ranking[index])
     if temp != -1:
         ranking[index] = temp
-
     GooogleScoreOriginData[index] = divideScoresIntoTwentyRegions(GooogleScoreOriginData[index])
     GoogleScoreGoogleTranslatedData[index] = divideScoresIntoTwentyRegions(GoogleScoreGoogleTranslatedData[index])
     GoogleScoreYandexTranslatedData[index] = divideScoresIntoTwentyRegions(GoogleScoreYandexTranslatedData[index])
     GoogleScoreBaiduTranslatedData[index] = divideScoresIntoTwentyRegions(GoogleScoreBaiduTranslatedData[index])
+
 
     BaiduAnalysisOriginDataGoogleStandard[index] = divideScoresIntoTwentyRegions(BaiduAnalysisOriginDataGoogleStandard[index])
     BaiduAnalysisGoogleTranslatedDataGoogleStandard[index] = divideScoresIntoTwentyRegions(BaiduAnalysisGoogleTranslatedDataGoogleStandard[index])
     BaiduAnalysisYandexTranslatedDataGoogleStandard[index] = divideScoresIntoTwentyRegions(BaiduAnalysisYandexTranslatedDataGoogleStandard[index])
     BaiduAnalysisBaiduTranslatedDataGoogleStandard[index] = divideScoresIntoTwentyRegions(BaiduAnalysisBaiduTranslatedDataGoogleStandard[index])
 
-tempIndex = ranking.size
 for index in range(0, ranking.size):
     heatmap[int(GooogleScoreOriginData[index])][int(ranking[index])] = heatmap[int(GooogleScoreOriginData[index])][int(ranking[index])] + 1
 
 print (heatmap)
+for y in range (0, 5):
+    newX = 19
+    for x in range (0, 20):
+        newHeatmap[x][y] = heatmap[newX][y]
+        newX = newX - 1
+print (newHeatmap)
